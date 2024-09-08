4f650a3f3b3883b6aaa2cfbe03d0efd305afeca7
Dimanerter <erterujamp@gmail.com>
2024-09-08 22:08:55 +0200

    sort_lists

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-my_list = [1, 2,3,4,5]
-print(len(my_list))
+nums = [3,1,4,1,5,9,2]
+sorted_nums = sorted(nums)
+print(sorted_nums)
+
+sorted_nums_desc = sorted(nums, reverse=True)
+print(sorted_nums_desc)
+
+words = ["banana", "apple", "cherry"]
+sorted_words = sorted(words, key = len)
+print(sorted_words)
