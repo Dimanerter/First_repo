6b74b7598ab84d9a041a4bcc02cd74adeb9a954a
Dimanerter <erterujamp@gmail.com>
2024-09-09 16:04:50 +0200

    frozenset

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-a = {1, 2, 3}
-b = {3, 4, 5}
-# print(a.intersection(b))  # {3}
-# print(a & b)  # {3}
+a = frozenset([1, 2, 3])
+b = frozenset([3, 4, 5])
 
-print(a.intersection(b))
-print(a & b)
+union = a | b  # Об'єднання множин
+intersection = a & b  # Перетин множин
+difference = a - b  # Різниця множин
+symmetric_difference = a ^ b  # Симетрична різниця
 
-print(a.difference(b))
-print(a - b)
-
-print(a ^ b)
-print(a.symmetric_difference(b))
-
-print(a.union(b))
-print(a | b)
+print(union)  # frozenset({1, 2, 3, 4, 5})
+print(intersection)  # frozenset({3})
+print(difference)  # frozenset({1, 2})
+print(symmetric_difference)  # frozenset({1, 2, 4, 5})
